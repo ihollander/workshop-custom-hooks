d415f63874b8ef6dd9aeb61de8b135b810b4504a
ihollander <ianholla@gmail.com>
2021-03-09 12:55:29 -0500

    After workshop

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,21 +2,22 @@ import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 
 /* ✅ modify this usePokemon custom hook to take in a query as an argument */
-export function usePokemon() {
+export function usePokemon(query) {
   /* ✅ this hook should only return one thing: an object with the pokemon data */
-}
-
-function Pokemon({ query }) {
-  /* 
-   ✅ move the code from the useState and useEffect hooks into the usePokemon hook
-   then, call the usePokemon hook to access the pokemon data in this component
-  */
   const [pokemon, setPokemon] = useState(null);
+
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
       .then(r => r.json())
       .then(setPokemon);
   }, [query]);
+
+  return { data: pokemon };
+}
+
+function Pokemon({ query }) {
+  const { data } = usePokemon(query);
+  const pokemon = data;
 
   // 🚫 don't worry about the code below here, you shouldn't have to touch it
   if (!pokemon) return <h3>Loading...</h3>;
@@ -60,6 +61,7 @@ const Wrapper = styled.section`
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   background: papayawhip;
   text-align: center;
+  max-width: 400px;
 
   h1 {
     background: #ef5350;
--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,35 +1,23 @@
 import { useEffect, useState } from "react";
 
-/* 
-  the two parameters for this function are: 
-  - key: the key on localStorage where we are saving this data
-  - initialValue: the initial value of state
-*/
 export function useLocalStorage(key, initialValue) {
-  /* 
-    ✅ in this hook, use the useState hook. For the initial value for state:
-    use the value saved in localStorage OR the initialValue from the function parameters 
-  */
+  const localStorageValue = JSON.parse(localStorage.getItem(key));
 
-  /* 
-   ✅ write a useEffect hook 
-   in the useEffect, when state is updated, save the state to localStorage
-   don't forget the dependencies array!
-  */
-  useEffect(() => {});
+  const [state, setState] = useState(localStorageValue || initialValue);
 
-  /* 
-   ✅ return the same interface as useState:
-   an array with state and a setState function
-  */
-  // 👀 return [state, setState]
+  useEffect(() => {
+    const stringified = JSON.stringify(state);
+    localStorage.setItem(key, stringified);
+  }, [state, key]);
+
+  return [state, setState];
 }
 
 function Form() {
   // ✅ after implementing the useLocalStorage hook, replace useState with useLocalStorage
   // don't forget to pass in both arguments (a key and an initialValue)
-  const [name, setName] = useState("");
-  console.log(name);
+  const [name, setName] = useLocalStorage("name", "");
+  // console.log(name);
 
   return (
     <form style={{ display: "flex", flexDirection: "column" }}>
@@ -42,7 +30,7 @@ function Form() {
 
 function FormWithObject() {
   // 🤓 save me for the bonus! when you're ready, update this useState to use your useLocalStorage hook instead
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useLocalStorage("form_object2", {
     title: "",
     content: "",
   });
